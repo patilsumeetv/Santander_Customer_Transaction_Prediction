eb1152b2def66ab0dce2f30b2ea3627bbc75a110
Sumeet Patil <patil.sumeet.v@gmail.com>
2019-05-08 11:14:31 +0530

    preprocessing

--- Code/process_functions.py
+++ Code/process_functions.py
@@ -13,3 +13,11 @@ def data_reader(data_path):
     test_features = test_df.drop(columns=['ID_code'])
     
     return train_features, train_target, test_features, test_idcode
+
+def train_test_loader(data_path):
+    train_features = pd.read_csv(os.path.join(data_path, "train_features.csv"))
+    train_target = pd.read_csv(os.path.join(data_path, "train_target.csv"), header=None)
+    test_features = pd.read_csv(os.path.join(data_path, "test_features.csv"))
+    test_target = pd.read_csv(os.path.join(data_path, "test_target.csv"), header=None)
+    
+    return train_features, train_target, test_features, test_target
